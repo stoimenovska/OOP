f4ef00fad8c52f181c8c61040d47f9a6411cbf2b
Marina Stoimenovska <44378752+stoimenovska@users.noreply.github.com>
2019-03-15 13:47:34 +0200

    Create main.cpp

---
+++ week2/main.cpp
@@ -0,0 +1,30 @@
+#include "Header.h"
+#include <iostream>
+
+int main() {
+
+	int N;
+	std::cin >> N;
+
+	Exhibition* exs = new Exhibition[N];
+	int M, T;
+
+	for (int i = 0; i < N; i++) {
+		std::cin >> M >> T;
+
+		exs[i] = Exhibition(T);
+
+		for (int j = 0; j < M; j++) {
+
+			int id;
+			double value;
+			char name[100], author[100];
+			std::cin >> id >> author >> name >> value;
+			Picture p(id, author, name, value);
+			exs[i].addPicture(p);
+		}
+		
+	}
+
+
+}
